2af0ef2966319d77583a96814a4d1d0636056606
jdbogieman <justin.diblasi@gmail.com>
2016-03-23 16:25:41 -0500

    Fibonacci Sequence

---
+++ FibonacciProblem/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciProblem
+{
+    class Fibonacci
+    {
+
+
+        public Fibonacci(int ceiling)
+        {
+            int a;
+            int b;
+            int c;
+
+            for (b = 1, c = 0; c <= ceiling; c = a + b)
+            {
+                a = b;
+                b = c;
+                Console.Write(b + ",");
+            }
+        }
+
+    }
+}
---
+++ FibonacciProblem/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciProblem
+{
+    class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            Fibonacci myFibonacci = new Fibonacci(1000);
+            Console.ReadLine();
+        }
+    }
+
+}
